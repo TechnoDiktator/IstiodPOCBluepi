0a75a37bcac1f82931442d62229ef87fd2a61840
technoDiktator <rastogitarang5@gmail.com>
2025-02-11 00:31:21 +0530

    models file changedd

--- models/Product.go
+++ models/Product.go
@@ -1,9 +1,19 @@
 package models
 
+import (
+	"time"
+)
+
 type Product struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Price int    `json:"price"`
 }
 
-
+type ProductWithMetadata struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Price       int       `json:"price"`
+	RequestedAt time.Time `json:"requested_at"`
+	ServiceAID  string    `json:"service_a_id"`
+}
