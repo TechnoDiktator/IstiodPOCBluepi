982382d3e7d7077ace2ba5d3307c62992f9a8308
technoDiktator <rastogitarang5@gmail.com>
2025-02-11 17:04:59 +0530

    no change

--- cmd/serviceA/ServiceA.go
+++ cmd/serviceA/ServiceA.go
@@ -11,6 +11,18 @@ import (
 	"github.com/yourusername/IstiodPOCBluepi/models"
 )
 
+// {
+// 	"sub": "google-oauth2|104494965273019075736",
+// 	"given_name": "Tarang ",
+// 	"family_name": "Rastogi",
+// 	"nickname": "tarang",
+// 	"name": "Tarang Rastogi",
+// 	"picture": "https://lh3.googleusercontent.com/a-/ALV-UjVQ_lqYpdFVvlkchRr6AzwOjtnopQ2EMzIjUY1f_wV54pqD7zEpqSZMyLfeG97iZyiEJvvKnX1b7mXGvYlqfQ59JvwNOo7k_CLZ8xoMk0OlRFrvE4-oa6ypz05rHpoFcbxHN_37YOt128pMMfcCz8BMmwp45lUq5WkiDGKJseO0TANCd0hKWUytNTKm_JGsu4T-FiAe8AvfLdSeuACwe4oD-SANVBZipHQOcput8DUaLalnX1zgc9Xd7xIeps2f2ER8aBaxxNmS8rbc-ieivKHupjh_yWJ0FbcT567ox_0Z1udvp2VT5eeWrCs8d9IRJ8W_PN_CoUn3oH8Z8Ak6d1Efy2gQsDeLC2BME5yOOUa2ns8rERQ9SjphWKTmsO02D3qoK8hBPh3_ZMeFNU0mwUmMJmeR-iDfS4VqxfQnpOiVmQTCoKvOtw5FxjSCdrWjHPZxSThXVi4xMndF9je3x05wBGTaLUdSyma-I_V_iPPmgOkmvpuyOVEtuwRKMJzQZTu3sUVMJRqQKM3uwxu6fjhhVOnCl-JeCLlEBcTlLwgfb4-QlNSkwKDCdduR3zqY-6qnwABqhRDwXYgV8E3JVQ3d4p8vLZ8zfS-O7gmph_QZGE4vR48KxaMrgFx5Gx3z-uvzuorGwu0ZkafcWGAQ0TSnqFxwRj8e-wwmxFRc34G2jFHv6jXHXKxwfhrge15YNepWvplK9pq7ChWESFO1WIHsAuqn6qq-KF52OtmB_PdVqn4gvbE3P5QqcJxtDmCvePOEeuy290SgKdDWJ_pBnTDQgraJfJ_nlCNoo2NqOc6xUA2ToL2j6iQC-TlXi795ptFbo58vV7yVMXkrqmauSqUkyWrzP9M73EQXMhFITgbnUhBcb3ixfr4_f_c5NFIa-bfeF50MrV-frZG0m7-WZY_lvn_CBEPkfE8u7p0WknInV84Unhy_CCuXasRsbB-mk5cdbHyT21JrxpcRUzpWoNPhJ6w=s96-c",
+// 	"updated_at": "2025-02-11T10:52:14.720Z"
+//   }
+
+
+
 type ServiceA struct {
 	ServiceBURL string
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/auth0/go-jwt-middleware/v2 v2.2.2 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -21,9 +20,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
-	github.com/go-resty/resty/v2 v2.16.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
